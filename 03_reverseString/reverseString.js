c997b7806d006b78076c44a6e79de591ef605f42
ghetto_duke777@hotmail.com <148827156+ghettoduke777@users.noreply.github.com>
2025-06-05 21:18:02 -0400

    edit reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let text = "Hello World Reversed";
+  string = text;
+  let result = string.split("").reverse(text);
+  return result.join("");
+}
 };
 
 // Do not edit below this line
