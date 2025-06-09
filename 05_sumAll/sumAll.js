5958ed3f752dd861b3f8f9540649ff4e03bc810a
ghetto_duke777@hotmail.com <148827156+ghettoduke777@users.noreply.github.com>
2025-06-08 22:10:22 -0400

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let sum = "";
+    const z = x + y;
+      sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;
 };
+console.log(sumAll(0,2));
 
 // Do not edit below this line
 module.exports = sumAll;
