0780471592970a8ae32f4454ff1ceddd2dbaa548
ghetto_duke777@hotmail.com <148827156+ghettoduke777@users.noreply.github.com>
2025-06-12 19:26:38 -0400

    Add Edits to tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+ 
 };
+// Function to convert Fahrenheit to Celsius
+const convertToFahrenheit = function(celcius) {
+   return (celsius * 9/5) + 32;
+};
+// Function to convert Celsius to Fahrenheit
+function tempToFahrenheit(celsius) {
+    return (fahrenheit - 32) * 5/9;
+}
 
-const convertToFahrenheit = function() {
-};
+
+// Example calls
+console.log(tempToFahrenheit(25)); // Output: 77
+console.log(tempToCelsius(77)); // Output: 25
 
 // Do not edit below this line
 module.exports = {
